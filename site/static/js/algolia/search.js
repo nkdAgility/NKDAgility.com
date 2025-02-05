881f058141046d201444a199a2490893381aea3b
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2025-02-05 11:46:59 +0000

    ✨ (search): integrate Algolia InstantSearch with custom styling
    Integrate Algolia's InstantSearch.js to enhance the search functionality
    with a more interactive and visually appealing search experience. The
    changes include adding custom styles for the search interface, improving
    the user interface with a search box, hits display, and a powered-by
    footer. The search index is updated to "NKDA-WEB-nkdagility_com_pages"
    for more accurate results. This integration aims to provide users with
    a seamless and efficient search experience on the website.

--- site/static/js/algolia/search.js
+++ site/static/js/algolia/search.js
@@ -1,28 +1,185 @@
+import { liteClient as algoliasearch } from "algoliasearch/lite";
+import { searchBox, hits, configure, poweredBy } from "instantsearch.js/es/widgets";
+
+// Add styles
+const styles = document.createElement("style");
+styles.textContent = `
+  .ais-InstantSearch {
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
+    box-shadow: 0 0 #0000, 0 0 #0000, 0px 0px 0px 1px rgba(35, 38, 59, 0.05), 0px 1px 3px 0px rgba(35, 38, 59, 0.15);
+    background-color: rgb(255 255 255);
+  }
+
+  .ais-SearchBox-form {
+    position: relative;
+  }
+
+  .ais-SearchBox-input {
+    width: 100%;
+    box-shadow: none;
+    border: none;
+    padding: 14px 40px;
+  }
+
+  .ais-SearchBox-submit,
+  .ais-SearchBox-reset {
+    position: absolute;
+    top: 0;
+    height: 100%;
+    background: none;
+    border: none;
+    appearance: none;
+  }
+
+  .ais-SearchBox-submit {
+    left: 0;
+    width: 40px;
+  }
+
+  .ais-SearchBox-reset {
+    right: 12px;
+  }
+
+  .ais-Hits-item {
+    padding: 0.5rem;
+    display: flex;
+    align-items: center;
+    gap: 1.25rem;
+    box-shadow: none;
+  }
+
+  .ais-Hits-item picture  {
+    height: 96px;
+    width: 96px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+  }
+
+  .ais-Hits-item img {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: cover;
+  }
+
+  .ais-Hits-item p {
+    margin-bottom: 0.1rem;
+    word-break: break-all;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+  }
+
+  .ais-Hits-item .secondary-text {
+    -webkit-line-clamp: 2;
+  }
+
+  .ais-Hits-item .primary-text {
+    margin-top: 0;
+    font-weight: 700;
+  }
+
+  .ais-Hits-item .tertiary-text {
+    font-size: 0.8rem;
+    color: rgb(90, 94, 154);
+  }
+
+  .ais-Hits-item mark {
+    color: #003dff;
+    background-color: #f2f4ff;
+    font-style: normal;
+  }
+
+  .ais-SearchBox-submitIcon {
+    width: 15px;
+    height: 15px;
+  }
+
+  .border-top {
+    border-style: solid;
+    border-width: 0;
+    border-top-width: 1px;
+    border-color: rgb(214 214 231);
+  }
+
+  #hits {
+    padding: 1rem;
+  }
+
+  #algolia-footer {
+    display: flex;
+    justify-content: flex-end;
+    padding: 16px;
+  }
+
+  .hide-content {
+    display: none !important;
+  }
+`;
+document.head.appendChild(styles);
+
+// Query for any other node in which you want to display the search bar
+const searchBarContainer = document.querySelector("body");
+const searchBarNode = document.createElement("div");
+searchBarNode.setAttribute("class", "ais-InstantSearch");
+
+searchBarNode.innerHTML = `
+  <div id="searchbox"></div>
+  <div id="hits" class="hide-content"></div>
+  <div id="algolia-footer" class="hide-content"></div>
+`;
+
+searchBarContainer.prepend(searchBarNode);
+
 const searchClient = algoliasearch("RSKJJ82EIK", "8b32e7e6c23096e5e958ddc581c14aee");
 
 const search = instantsearch({
-  indexName: "pages",
+  indexName: "NKDA-WEB-nkdagility_com_pages",
   searchClient,
+  onStateChange({ uiState, setUiState }) {
+    const hitsContainer = document.querySelector("#hits");
+    const footerContainer = document.querySelector("#algolia-footer");
+
+    if (!uiState["NKDA-WEB-nkdagility_com_pages"]?.query) {
+      hitsContainer.classList.add("hide-content");
+      footerContainer?.classList.add("hide-content");
+      setUiState(uiState);
+      return;
+    }
+
+    hitsContainer.classList.remove("hide-content");
+    footerContainer?.classList.remove("hide-content");
+    setUiState(uiState);
+  },
 });
 
 search.addWidgets([
-  instantsearch.widgets.searchBox({
+  searchBox({
     container: "#searchbox",
+    placeholder: "Search your data here",
   }),
-
-  instantsearch.widgets.hits({
+  configure({
+    hitsPerPage: 3,
+  }),
+  poweredBy({
+    container: "#algolia-footer",
+  }),
+  hits({
     container: "#hits",
     templates: {
-      item: `
-            <article>
-          <p>Name: {{#helpers.highlight}}{ "attribute": "title", "highlightedTagName": "mark" }{{/helpers.highlight}}</p>
-        </article>
-          `,
+      item: (hit, { html, components }) => html`
+        <picture>
+          <img src="${hit.image}" />
+        </picture>
+        <div>
+          <p class="primary-text">${components.Highlight({ hit, attribute: "title" })}</p>
+        </div>
+      `,
     },
-  }),
-  instantsearch.widgets.refinementList({
-    container: "#refinement",
-    attribute: "company",
   }),
 ]);
 
