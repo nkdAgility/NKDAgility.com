66ad0276cefd4a42e5ccac08665ca57181032fb8
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2025-02-09 11:21:43 +0000

    Update

--- .powershell/_includes/ClassificationHelpers.ps1
+++ .powershell/_includes/ClassificationHelpers.ps1
@@ -272,16 +272,3 @@ do not wrap the json in anything else, just return the json object.
 
 
 
-
-# $hugoMarkdown = Get-HugoMarkdown -Path "site\content\resources\blog\2011\2011-02-04-do-you-know-about-the-visual-studio-alm-rangers-guidance\index.md"
-# $cacheFolder = "site\content\resources\blog\2011\2011-02-04-do-you-know-about-the-visual-studio-alm-rangers-guidance\"
-
-# $classification = "tags"
-# $catalogHash = Get-CatalogHashtable -Classification $classification
-# $class = Get-BatchCategoryConfidenceWithChecksum -ClassificationType "$classification" -Catalog $catalogHash -CacheFolder $cacheFolder -ResourceContent $hugoMarkdown.BodyContent -ResourceTitle $hugoMarkdown.FrontMatter.title
-
-
-# $class.Count
-# $class
-
-# Write-InfoLog "OpenAI.ps1 loaded"
