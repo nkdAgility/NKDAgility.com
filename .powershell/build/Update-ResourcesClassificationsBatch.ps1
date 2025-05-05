b2b0cedcc6a7c87df69321ba03c18632e8720f9b
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2025-05-05 15:12:40 +0100

    ✨ (HugoHelpers.ps1, Update-ResourcesClassificationsBatch.ps1): enhance markdown resource retrieval and classification update
    Update the `Get-RecentHugoMarkdownResources` function to include both "index.md" and "_index.md" files, broadening the scope of markdown files retrieved. This change ensures that all relevant markdown files are considered, not just those named "index.md".
    
    In `Update-ResourcesClassificationsBatch.ps1`, extend the retrieval of markdown resources to include additional directories: "concepts", "categories", and "tags". This allows for a more comprehensive update of resource classifications, ensuring that all relevant content is processed and up-to-date.

--- .powershell/build/Update-ResourcesClassificationsBatch.ps1
+++ .powershell/build/Update-ResourcesClassificationsBatch.ps1
@@ -10,6 +10,9 @@ $ErrorActionPreference = 'Stop'
 $levelSwitch.MinimumLevel = 'Information'
 
 $hugoMarkdownObjects = Get-RecentHugoMarkdownResources -Path ".\site\content\resources" -YearsBack 1
+$hugoMarkdownObjects += Get-RecentHugoMarkdownResources -Path ".\site\content\concepts" -YearsBack 10
+$hugoMarkdownObjects += Get-RecentHugoMarkdownResources -Path ".\site\content\categories" -YearsBack 10
+$hugoMarkdownObjects += Get-RecentHugoMarkdownResources -Path ".\site\content\tags" -YearsBack 10
 
 ##############################################
 Update-ClassificationsForHugoMarkdownList -hugoMarkdownList $hugoMarkdownObjects
