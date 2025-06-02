678f4a278b0179cdeaa2e1c45260c895f20d16f7
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2025-06-02 14:42:11 +0100

    ``` 🐛 fix(build): correct path in Update-ResourcesFrontMatter script
    - fix incorrect path by adding a trailing slash to the resources directory
    
    ✨ feat(case-studies): add new case study for engineering excellence
    
    - introduce new case study detailing SLB and NKD Agility's engineering transformation
    - includes comprehensive executive summary, client challenges, strategic approach, and measurable outcomes
    
    ✨ feat(ui): add icon for case studies in resource component
    
    - integrate new icon specifically for case studies to enhance visual representation
    ```

--- .powershell/build/Update-ResourcesFrontMatter.ps1
+++ .powershell/build/Update-ResourcesFrontMatter.ps1
@@ -14,7 +14,7 @@ $ResourceAliasExpiryDate = (Get-Date).Date.AddYears(-5)
 
 Start-TokenServer
 
-$hugoMarkdownObjects = Get-RecentHugoMarkdownResources -Path ".\site\content\resources" -YearsBack 10
+$hugoMarkdownObjects = Get-RecentHugoMarkdownResources -Path ".\site\content\resources\" -YearsBack 10
 
 Write-InformationLog "Processing ({count}) HugoMarkdown Objects." -PropertyValues ($hugoMarkdownObjects.Count)
 ### /FILTER hugoMarkdownObjects
